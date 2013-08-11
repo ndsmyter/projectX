39918a5b89fa3c8a819405898fd272d495508488
stevedc1989 <de.clercq.steve1@gmail.com>
2013-08-11 13:42:33 +0200

    minor bug fixes

--- odysseus/src/main/java/com/projectx/App.java
+++ odysseus/src/main/java/com/projectx/App.java
@@ -10,8 +10,8 @@ public class App extends JFrame
 {
     public static void main( String[] args )
     {
-        System.setProperty("http.proxyHost", "proxy.houston.hp.com");
-        System.setProperty("http.proxyPort", "8080");
+        //System.setProperty("http.proxyHost", "proxy.houston.hp.com");
+        //System.setProperty("http.proxyPort", "8080");
         MainScreen start=new MainScreen();
         start.setVisible(true);
 
--- odysseus/src/main/java/com/projectx/MapShower.java
+++ odysseus/src/main/java/com/projectx/MapShower.java
@@ -3,6 +3,7 @@ package com.projectx;
 import org.openstreetmap.gui.jmapviewer.DefaultMapController;
 import org.openstreetmap.gui.jmapviewer.JMapViewer;
 import org.openstreetmap.gui.jmapviewer.MapMarkerDot;
+import java.awt.event.MouseEvent;
 
 /**
  * @author steve
@@ -13,5 +14,11 @@ public class MapShower {
 
     }
     public JMapViewer Viewer(){
+        JMapViewer mapViewer=new JMapViewer();
+        mapViewer.repaint();
+        DefaultMapController mapController = new DefaultMapController(mapViewer);
+        mapController.setMovementMouseButton(MouseEvent.BUTTON1);
+        //mapViewer.setDisplayPositionByLatLon();
+        return  mapViewer;
     }
 }
